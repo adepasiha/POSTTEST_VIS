e9f8b382b3fec4555be9ebdf68b4cf2483c02777
Ade ganteng <pasihaade@gmail.com>
2026-02-26 13:53:40 +0800

    Add files via upload

---
+++ Posttest_1/Form1.vb
@@ -0,0 +1,66 @@
+﻿Public Class Form1
+
+    Dim totalIP As Double = 0
+    Dim jumlahSemester As Integer = 0
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+
+        Dim ip As Double
+
+        ' Validasi input
+        If Double.TryParse(txtIPSemester.Text, ip) Then
+
+            If ip >= 0 And ip <= 4 Then
+
+                totalIP += ip
+                jumlahSemester += 1
+
+                Dim ipk As Double = totalIP / jumlahSemester
+                txtIPKumulatif.Text = ipk.ToString("0.00")
+
+                ' Menentukan predikat
+                If ipk >= 2.0 And ipk <= 2.75 Then
+                    lblPredikat.Text = "Cukup"
+                ElseIf ipk >= 2.76 And ipk <= 3.0 Then
+                    lblPredikat.Text = "Memuaskan"
+                ElseIf ipk >= 3.01 Then
+                    lblPredikat.Text = "Sangat Memuaskan"
+                Else
+                    lblPredikat.Text = ""
+                End If
+
+                ' Kosongkan input
+                txtIPSemester.Clear()
+                txtIPSemester.Focus()
+
+            Else
+                MessageBox.Show("IP harus antara 0 - 4")
+            End If
+
+        Else
+            MessageBox.Show("Masukkan angka yang valid!")
+        End If
+
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+
+        totalIP = 0
+        jumlahSemester = 0
+
+        txtIPSemester.Clear()
+        txtIPKumulatif.Clear()
+        lblPredikat.Text = ""
+
+        txtIPSemester.Focus()
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+End Class
